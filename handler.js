44b49139bf0c6598118875927e5ba5d72bed2afd
Jaime Olmo <jamesxv7@gmail.com>
2015-02-26 17:09:33 -0400

    Create handler.js

---
+++ handler.js
@@ -0,0 +1,28 @@
+// function executed when the state of the request changes
+function handler()
+{
+	// Continue if the process is completed
+	if (ajax.readyState == 4)
+	{
+		// Continue only if HTTP status is "OK"
+		if (ajax.status == 200)
+		{
+			try
+			{
+			// Retrieve the response
+			response = ajax.responseText;
+			// Do something with the response
+			}
+			catch(e)
+			{
+			// Display error message
+			alert("Error reading the response: " + e.toString());
+			}
+		}
+	else
+	{
+	// Display status message
+	alert("There was a problem retrieving the data:\n" + ajax.statusText);
+	}
+	}
+}
